f64eada579ac309f8047cb8b98e6f62faee97fc9
sas <73313177+areyouprofessor@users.noreply.github.com>
2022-10-29 23:02:54 +0100

    Update slider.java

--- slider.java
+++ slider.java
@@ -2,54 +2,47 @@ class Slider{
   boolean here = true;
   int x; int y; 
   int c = 30; int d;
-  int w1; int h1;
-  int w2; int h2;
+  int w; int h;
   
   Slider (int x, int y, int w, int h){
     this.x = x;
     this.y = y;
-    this.w1 = w;
-    this.w2 = w-(w/100);
-    this.h1 = h;
-    this.h2 = h-(h/100);
-    this.d = h2;
+    this.w = w;
+    this.h = h;
+    this.d = (80*h)/100;
   }
   
   Slider (int x, int y, int w, int h, boolean here){
     this.here = here;
     this.x = x;
     this.y = y;
-    this.w1 = w;
-    this.w2 = w-20;
-    this.h1 = h;
-    this.h2 = h-20;
-    this.d = h1-10;
+    this.w = w;
+    this.h = h;
+    this.d = (80*h)/100;
   }
   
   void clicked() {
-    if ((mouseX >= x) && (mouseX <= x+w1) && (mouseY >= y) && (mouseY <= y+h1)) {
+    if ((mouseX >= x) && (mouseX <= x+w) && (mouseY >= y) && (mouseY <= y+h)) {
       here = true;
     }
     here = false;
   }
   void drawy(){
-    fill(navy);
-    rect(x,y,w1,h1,c);
     if(here){
       fill(blue);
-      rect(x + 10,y + 10,w2,h2,c);
+      rect(x,y,w,h,6000);
       stroke(black); //black
       fill(white); //white
-      strokeWeight(4);
-      ellipse(x+h1/2,y+h1/2,d,d);
+      strokeWeight(0.05*d);
+      ellipse(x+((50*h)/100),y+h/2,d,d);
       strokeWeight(0);
     }else{
-      fill(red); //red
-      rect(x + 10,y + 10,w2,h2,c);
-      stroke(black); //black
-      fill(white); //white
-      strokeWeight(4);
-      ellipse(w1+x-h1/2,y+h1/2,d,d);
+      fill(red);
+      rect(x,y,w,h,6000);
+      stroke(black);
+      fill(white);
+      strokeWeight(0.05*d);
+      ellipse(w+x-((50*h)/100),y+h/2,d,d);
       strokeWeight(0);
     }
   }
