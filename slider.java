e79ccdf54101c2038c82a416374e3b660bacdc2a
sas <73313177+areyouprofessor@users.noreply.github.com>
2022-10-29 22:20:40 +0100

    Update slider.java

--- slider.java
+++ slider.java
@@ -1,52 +1,55 @@
 class Slider{
-  boolean here = true; //slider value of here or not
-  int x; int y; int c = 20; int d = 65;
-  int w1 = 300; int h1 = 75; //width and height values of outer rect
-  int w2 = 280; int h2 = 55; //width and height values of inner rect
+  boolean here = true;
+  int x; int y; 
+  int c = 30; int d;
+  int w1; int h1;
+  int w2; int h2;
   
-  Slider (int x, int y){
-    //initialise within the page
-    this.x = 50 + x;
-    this.y = 150 + y;
+  Slider (int x, int y, int w, int h){
+    this.x = x;
+    this.y = y;
+    this.w1 = w;
+    this.w2 = w-(w/100);
+    this.h1 = h;
+    this.h2 = h-(h/100);
+    this.d = h2;
   }
   
-  Slider (int x, int y, boolean here){
+  Slider (int x, int y, int w, int h, boolean here){
     this.here = here;
-    this.x = 50 + x;
-    this.y = 150 + y;
+    this.x = x;
+    this.y = y;
+    this.w1 = w;
+    this.w2 = w-20;
+    this.h1 = h;
+    this.h2 = h-20;
+    this.d = h1-10;
   }
   
   void clicked() {
-    //edit truth value of slider
     if ((mouseX >= x) && (mouseX <= x+w1) && (mouseY >= y) && (mouseY <= y+h1)) {
       here = true;
     }
     here = false;
   }
-  
   void drawy(){
-    /*Outer Rectangle*/
     fill(navy);
-    rect(x,y,w1,h1,c,c,c,c);
+    rect(x,y,w1,h1,c);
     if(here){
-      /*Inner Rectangle*/
       fill(blue);
-      rect(x + 10,y + 10,w2,h2,c,c,c,c);
-      /*Button*/
-      stroke(black);
-      fill(white);
+      rect(x + 10,y + 10,w2,h2,c);
+      stroke(black); //black
+      fill(white); //white
       strokeWeight(4);
       ellipse(x+h1/2,y+h1/2,d,d);
       strokeWeight(0);
     }else{
-      /*Inner Rectangle*/
-      fill(red);
-      rect(x + 10,y + 10,w2,h2,c,c,c,c);
-      /*Button*/
-      stroke(black);
-      fill(white);
+      fill(red); //red
+      rect(x + 10,y + 10,w2,h2,c);
+      stroke(black); //black
+      fill(white); //white
       strokeWeight(4);
-      ellipse(w1+x-h1/2,y+h1/2,65,65);
+      ellipse(w1+x-h1/2,y+h1/2,d,d);
       strokeWeight(0);
     }
   }
